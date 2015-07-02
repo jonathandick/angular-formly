0670ec380dd9038abf91ec469b5c6ba41d881d55
jonathandick <jonathan.j.dick@gmail.com>
2015-07-02 17:52:00 +0300

    added ability to load pre-existing data, including obsGroups into form

--- scripts/MainController.js
+++ scripts/MainController.js
@@ -14,45 +14,101 @@
 
         // The model object that we reference
         // on the  element in index.html
-        vm.model = {obs:{}};
+        vm.model =
+        {
+            obs:
+            {
+                "durationConceptUuid":[{"concept":"durationConceptUuid",value:"100"}]
+            }
+        };
+
+        vm.model = {
+            "obs":
+            {
+                "durationConceptUuid":
+                    [
+                        {uuid:"12345", "concept":"durationConceptUuid","value":"100"}
+                    ],
+                "orderUuid":
+                {
+                    "obsGroup1":
+                    {
+                        "concept":"orderUuid",
+                        "obs": {
+                            medicineConceptUuid: [{uuid:"1987293", "concept": "medicineConceptUuid", "value": "quinineUuid"}],
+                            doseConceptUuid: [{uuid:"3298098", "concept": "doseConceptUuid", "value": "500mg"}]
+                        }
+                    }
+                }
+            }
+        };
         var o, obs;
 
+
+        function getLast(array) {
+            return array[array.length-1];
+        }
+
+        function getFirstNonLoaded(obs) {
+            var result;
+            _.each(obs,function(e) {
+                if(e.loaded === undefined) {
+                    result = e;
+                }
+            });
+            return result;
+        }
 
         /*
         params: obsConceptUuid, obsGroupUuid, groupName, isCheckbox, checkboxValue
          */
         function getModel(params) {
-            if(params.obsGroupUuid === undefined) {
-                obs = {concept:params.obsConceptUuid};
-                if(params.isCheckbox) obs.value = params.checkboxValue;
+            obs = {concept:params.obsConceptUuid};
+            if(params.isCheckbox) obs.value = params.checkboxValue;
 
+            if(params.obsGroupUuid === undefined) {
                 if(params.obsConceptUuid in vm.model.obs) {
-                    vm.model.obs[params.obsConceptUuid].push(obs);
+                    var e = getLast(vm.model.obs[params.obsConceptUuid]);
+                    if(e.loaded)
+                        vm.model.obs[params.obsConceptUuid].push(obs);
+                    else {
+                        e.loaded = true;
+                        e.initialValue = e.value;
+                        return e;
+                    }
                 }
                 else {
                     vm.model.obs[params.obsConceptUuid] = [obs];
                 }
-                o = (vm.model.obs[params.obsConceptUuid]);
-                return o[o.length-1];
+                return getLast((vm.model.obs[params.obsConceptUuid]));
             }
 
 
             if(vm.model.obs[params.obsGroupUuid] === undefined) {
                 vm.model.obs[params.obsGroupUuid] = {};
             }
-            if(params.groupName in vm.model.obs[params.obsGroupUuid])
-                (vm.model.obs[params.obsGroupUuid][params.groupName]).obs.push({concept:params.obsConceptUuid});
+
+            if(params.groupName in vm.model.obs[params.obsGroupUuid]) {
+
+                obs = vm.model.obs[params.obsGroupUuid][params.groupName].obs;
+                e = getFirstNonLoaded(obs[params.obsConceptUuid]);
+                if(!e)
+                    (vm.model.obs[params.obsGroupUuid][params.groupName]).obs.push({concept: params.obsConceptUuid});
+                else {
+                    e.loaded = true;
+                    e.initialValue = e.value;
+                    return e;
+                }
+
+            }
             else {
                 vm.model.obs[params.obsGroupUuid][params.groupName] =
                 {
                     concept: params.obsGroupUuid,
-                    obs: [{uuid: params.obsConceptUuid}]
+                    obs: [{concept: params.obsConceptUuid}]
                 };
             }
-            o = (vm.model.obs[params.obsGroupUuid][params.groupName]).obs;
-
-            return o[o.length-1];
-
+            return getLast((vm.model.obs[params.obsGroupUuid][params.groupName]).obs);
         }
         // An array of our form fields with configuration
         // and options set. We make reference to this in
@@ -61,11 +117,11 @@
 
         vm.fields = [
             {
-                key: 'checked',
-                type: 'checkbox',
-                model: getModel({obsConceptUuid:"durationConceptUuid",isCheckbox:true,checkboxValue:'100'}),
+                key: 'value',
+                type: 'input',
+                model: getModel({obsConceptUuid:"durationConceptUuid"}),
                 templateOptions: {
-                    type: 'checkbox',
+                    type: 'text',
                     label: 'Duration',
                     placeholder: 'Enter duration',
                     required: true
@@ -73,17 +129,37 @@
             },
 
             {
+                key: 'value',
+                type: 'input',
+                model: getModel({obsConceptUuid:"durationConceptUuid"}),
+                templateOptions: {
+                    type: 'text',
+                    label: 'Duration',
+                    placeholder: 'Enter duration',
+                    required: true
+                }
+            },
+
+
+            {
                 className: 'obsGroup1',
                 fieldGroup: [
                     {
                         key: 'value',
                         model: getModel({obsConceptUuid:"medicineConceptUuid",obsGroupUuid:"orderUuid",groupName:"obsGroup1"}),
-                        type: 'input',
+                        type: 'select',
                         templateOptions: {
-                            type: 'text',
                             label: 'Medicine',
                             placeholder: 'Enter medicine',
-                            required: true
+                            required: true,
+                            options:
+                                [
+                                    {name:"",value:""},
+                                    {name:"quinine",value:"quinineUuid"},
+                                    {name:"mefloquine",value:"mefloquineUuid"}
+                                ]
+
+
                         }
                     },
                     {
@@ -128,17 +204,6 @@
 
                 ]
             },
-            {
-                key: 'province',
-                type: 'select',
-                templateOptions: {
-                    label: 'Province/Territory',
-                    // Call our province service to get a list
-                    // of provinces and territories
-                    options: province.getProvinces()
-                },
-                //hideExpression: '!model.email'
-            }
         ];
 
     }
